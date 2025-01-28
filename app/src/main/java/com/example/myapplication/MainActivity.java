701e07bf4a9e4b3013f6a369a62824514eab3007
Lukkeezz23 <samec.luk@gmail.com>
2025-01-28 02:44:17 +0100

    Přidáno načítání  a ukládání - google sheets

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,12 +16,38 @@ public class MainActivity extends AppCompatActivity {
     Button btnExit;
 
     Button btnPorucha;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
+
+        sharedPreferences = getSharedPreferences("theme_prefs", MODE_PRIVATE);
+        boolean isDarkTheme = sharedPreferences.getBoolean("dark_theme", false);
+        if (isDarkTheme) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+
         EdgeToEdge.enable ( this );
         setContentView ( R.layout.activity_main );
+        Button btnToggleTheme = findViewById(R.id.btn_toggle_theme);
+        btnToggleTheme.setOnClickListener(v -> {
+            boolean currentTheme = sharedPreferences.getBoolean("dark_theme", false);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+
+            if (currentTheme) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                editor.putBoolean("dark_theme", false);
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                editor.putBoolean("dark_theme", true);
+            }
+
+            editor.apply();
+        });
+
         btnCreate = findViewById ( R.id.btn_create );
         btnExit = findViewById ( R.id.btn_exit );
         btnHistorie = findViewById ( R.id.btn_history );
